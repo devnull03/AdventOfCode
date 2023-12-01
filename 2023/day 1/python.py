fd9fc3bb6a352650d8a14c494cfe241dbcdbb521
devnull03 <56480041+devnull03@users.noreply.github.com>
2023-12-01 15:53:02 -0800

    fix: regex eeee

--- 2023/day 1/python.py
+++ 2023/day 1/python.py
@@ -1,6 +1,5 @@
 import re
 
-
 try:
     with open('input.txt') as file:
         l = [(i.rstrip('\n')) for i in file.readlines()]
@@ -9,12 +8,12 @@ except FileNotFoundError:
         l = [(i.rstrip('\n').split('')) for i in file.readlines()]
 
 # part 1
-# total = 0
-# for line in l:
-#     a = list(filter(lambda x: x.isnumeric(), line))
-#     total += int(a[0] + a[len(a) - 1])
+total = 0
+for line in l:
+    a = list(filter(lambda x: x.isnumeric(), line))
+    total += int(a[0] + a[len(a) - 1])
 
-# print(total)
+print(total)
 
 
 # part 2
@@ -31,16 +30,16 @@ digits = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"
 
 total = 0
 for line in l:
-    print(line)
-    reg = re.findall(r'(one|two|three|four|five|six|seven|eight|nine|\d)', line)
+    # print(line)
+    reg = re.findall(r'(?=(one|two|three|four|five|six|seven|eight|nine|\d))', line)
 
     for i in range(len(reg)):
         if reg[i] in digits:
             reg[i] = str(digits.index(reg[i]) + 1)
 
-    print(reg)
+    # print(reg)
     
-    print(int(reg[0] + reg[len(reg) - 1]))
+    # print(int(reg[0] + reg[len(reg) - 1]))
     total += int(reg[0] + reg[len(reg) - 1])
 
 print(total)
